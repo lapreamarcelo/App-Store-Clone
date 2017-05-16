e1beebf5eb4333de38804ec769db665c6046f302
Marcelo Laprea <marcelolaprea@Marcelos-MacBook-Pro.local>
2017-05-16 00:49:56 -0400

    Part 1. Ready

---
+++ AppStore/AppStore/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Marcelo Laprea on 5/16/17.
+//  Copyright © 2017 AvilaTek. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Background color
+        collectionView?.backgroundColor = UIColor.white
+        //For CollectionView we have to register the cell that we created
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CategoryCell
+        return cell!
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    //We can change de height and width of the cells with this methods
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+    
+
+}
